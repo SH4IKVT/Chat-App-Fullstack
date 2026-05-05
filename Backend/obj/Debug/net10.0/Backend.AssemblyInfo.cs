a280a2993b80223605341893945e51414775fe5f
SH4IKVT <shaikat1sarkar@gmail.com>
2026-05-05 16:32:56 +0530

    The working but future work is session storage

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6a3aadb3da7547a9b4d7ae1d21844149147b0d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72bed4501931650563752a35514df06e35422e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
